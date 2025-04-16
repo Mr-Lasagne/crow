007fac7c45a798639ac84a0591ae2b6672053b0e
Jack Linaker <toastlinaker@rocketmail.com>
2025-04-16 15:10:28 +0100

    chore: remove unnecessary files and folders

--- version2_flask/Unit_Testing/tests/test_arithmetic_mean.py
+++
@@ -1,8 +0,0 @@
-# Import the pytest function
-
-# We import the function
-from functions.arithmetic_mean import arithmetic_mean
-
-
-def test_all_ones():
-    assert arithmetic_mean([1, 1, 1, 1]) == 1
